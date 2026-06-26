d5a6293748e58b9a51bc9c569ee520133897e88b
Anitha11012 <anitha.ram1112@outlook.com>
2026-06-26 21:45:26 +0530

    Update API proxy targets to production URLs

--- services/api-gateway/src/server.js
+++ services/api-gateway/src/server.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(
   "/api/resume",
   createProxyMiddleware({
-    target: "http://localhost:5002",
+    // target: "http://localhost:5002",
+    target:"https://resume-analyzer-resume-service.onrender.com"
     changeOrigin: true,
     pathRewrite: {
       "^/api/resume": "/api/resume",
@@ -23,7 +24,8 @@ app.use(
 app.use(
   "/api/analysis",
   createProxyMiddleware({
-    target: "http://localhost:5003",
+    // target: "http://localhost:5003",
+    target:"https://resume-analyzer-2-g0je.onrender.com"
     changeOrigin: true,
     pathRewrite: {
       "^/api/analysis": "/api/analysis",
