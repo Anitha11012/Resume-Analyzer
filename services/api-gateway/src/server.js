6c7d6e917ecdd546d8c7208c4b2fd155e78f8828
Anitha11012 <anitha.ram1112@outlook.com>
2026-08-26 14:32:24 +0530

    Update API targets to new Azure endpoints

--- services/api-gateway/src/server.js
+++ services/api-gateway/src/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(
   "/api/resume",
   createProxyMiddleware({
-    target:"https://resume-analyzer-resume-service.onrender.com",
+    target:"https://resume-service-dfbrf0gna6fkeyfq.indiasouthcentral-01.azurewebsites.net",
     changeOrigin: true,
   })
 );
@@ -20,7 +20,7 @@ app.use(
 app.use(
   "/api/analysis",
   createProxyMiddleware({
-    target:"https://resume-analyzer-2-g0je.onrender.com",
+    target:"https://analysis-service-c5gkheb6c9d9h6ga.indiasouthcentral-01.azurewebsites.net",
     changeOrigin: true,
   })
 );
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    "API Gateway running on 5000"
+    `API Gateway running on ${PORT}`
   );
 });
 
